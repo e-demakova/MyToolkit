5de5ce1ce0d67a58413236307df2060bacd1039e
e-demakova <demakova@users.noreply.github.com>
2021-06-01 14:55:34 +0700

    Удаление мусора

--- Assets/com.deblue.toolkit/Runtime/GameProcess/GameModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Deblue.GameProcess
-{
-    public abstract class GameModel : PersistentMono<GameModel>
-    {
-        public static GlobalGameEvents _events = new GlobalGameEvents();
-        public static IGlobalGameEvents Events => _events;
-    }
-}
